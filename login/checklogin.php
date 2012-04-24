6a4024dfb36816ce702604c12427e2520126d833
root <root@simon-laptop.(none)>
2012-04-23 21:59:01 -0400

    got session variables to actually display

--- login/checklogin.php
+++ login/checklogin.php
@@ -38,7 +38,6 @@ $count=mysql_num_rows($result);
 if($count==1){
 // Register $myusername, $mypassword and redirect to file "login_success.php"
 session_register("myusername");
-session_register("mypassword"); 
 header("location:login_success.php");
 }
 else {
