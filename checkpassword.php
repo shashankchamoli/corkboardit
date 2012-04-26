7073ff303014b057a38ead9f69ddb71ff86dc7f6
root <root@simon-laptop.(none)>
2012-04-25 22:40:28 -0400

    ...

--- checkpassword.php
+++ checkpassword.php
@@ -23,7 +23,7 @@ mysql_select_db("$db_name")or die("cannot select DB");
 
 <?php
 $email=$_POST['email']; 
-$title=$_GET['title'];
+$title=$_POST['title'];
 $corkpass=$_POST['cork_pass'];
 
 echo $email."<br />";
